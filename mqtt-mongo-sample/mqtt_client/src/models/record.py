a117d8a7232c6a9fad00f79f2f55eb744a5aac07
Freddy <fredsossa1@gmail.com>
2020-06-15 03:46:59 +0800

    FS : Updating record Model by removing names

--- mqtt-mongo-sample/mqtt_client/src/models/record.py
+++ mqtt-mongo-sample/mqtt_client/src/models/record.py
@@ -6,7 +6,5 @@ class Record (Document):
 
     id_number = StringField(required=True, unique = True )
     location = StringField(required=True )
-    first_name = StringField(required=True )
-    last_name = StringField(required=True )
     body_temperature = FloatField(required=True )
     date = DateTimeField(default=datetime.datetime.now)
